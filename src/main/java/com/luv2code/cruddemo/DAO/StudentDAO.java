ebbeb54243e337b91d7527863c959c905b9326cd
Mostafa Esmail <100853809+MostafaEsma3il@users.noreply.github.com>
2024-08-16 22:38:41 +0300

    Update StudentDAO.java

--- src/main/java/com/luv2code/cruddemo/DAO/StudentDAO.java
+++ src/main/java/com/luv2code/cruddemo/DAO/StudentDAO.java
@@ -1,14 +1,14 @@
-package com.luv2code.cruddemo.DAO;
-
-import com.luv2code.cruddemo.entity.Student;
-import org.springframework.objenesis.strategy.StdInstantiatorStrategy;
+package com.mostafaesmail.cruddemo.dao;
 
 import java.util.List;
+import com.mostafaesmail.cruddemo.entity.Student;
 
 public interface StudentDAO {
     void save(Student student);
     Student findById(int id);
     List<Student> findAll();
-    List<Student> findByLastName(String theLastName);
+    List<Student> findByLastName(String lastName);
     void update(Student student);
+    void delete(int id);
+    int deleteAll();
 }
